d823ccd1a1f6b6620c3085c23da9dbad7e8937b8
Thomas Munzar <tmunzar92@gmail.com>
2023-08-02 20:08:25 -0700

    added seeds, getting repeat data when i look in terminal. Need to fix this.

--- db/schema.sql
+++ db/schema.sql
@@ -9,22 +9,22 @@ USE employees_db;
 
 CREATE TABLE department(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30))
+    name VARCHAR(30));
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    dep_id INT, --Foreign Key
-    FOREIGN KEY (dep_id) REFERENCES department(id)); 
+    dept_id INT, 
+    FOREIGN KEY (dept_id) REFERENCES department(id)); 
 
  CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id, -- THIS IS A FOREIGN KEY ***
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee(id)
-     );-- THIS IS A FOREIGN KEY ****
+     );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,27 @@
 -- INSERT INTO ROLE TABLE
 -- INTERT INTO EMPLOYEE TABLE
 
+
+INSERT INTO department(name)
+VALUES ('Chartoon Airlines'),
+       ('Culinary Department'),
+       ('Plane Fixing Shop');
+      
+
+
+
+INSERT INTO roles (title, salary, dept_id)
+VALUES ('CEO', 5000, 1),
+       ('Chef', 80000, 2),
+       ('Flight Attendant', 7000, 1),
+       ('Pilot', 10000, 1),
+       ('Mechanic', 4000, 3),
+       ('Co-Pilot', 50000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Nelson', 'Muntz', 6),
+       ('Marge', 'Simpson', 5),
+       ('Milhouse', 'Van Houten', 3),
+       ('Edna', 'Krabappel', 4),
+       ('Barney', 'Gumble', 2),
+       ('Hans', 'Moleman', 1);
